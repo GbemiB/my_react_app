4fb535219224fefa6c76e94171c484232f5516ba
Taiwo  Gbemisola Bello <tbello@ecobank.com>
2022-07-03 01:43:36 +0100

    Registration Interface

--- src/App.js
+++ src/App.js
@@ -1,7 +1,33 @@
-function App(){
-  return(
-      <h1>Hello</h1>
-  )
+import React from 'react';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import Header from './component/Header';
+import OnboardingForm from './component/OnboardingForm';
+
+function App() {
+  return (
+
+    <Router>
+
+        <div className='container'>
+            <Routes>
+                <Route
+                    path='/'
+                    element={
+                        <>
+                            <Header />
+                            <OnboardingForm />  
+                           
+                        </>
+                    }
+                ></Route>
+                {/* <Route path='/about' element={<AboutPage />} /> */}
+            </Routes>
+
+            {/* <AboutIconLink /> */}
+        </div>
+    </Router>
+)
+
 }
 
 export default App
